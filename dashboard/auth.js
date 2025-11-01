4ef87ab60580445d782fd0bde07c7f0b12a5a791
Jeronimo <junior.karaseks@gmail.com>
2025-11-01 16:35:23 -0300

    v1.0.1

--- dashboard/auth.js
+++ dashboard/auth.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = '';
-const SUPABASE_ANON_KEY = '';
+const SUPABASE_URL = 'https://gpakoffbuypbmfiwewka.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdwYWtvZmZidXlwYm1maXdld2thIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI0ODA2NjAsImV4cCI6MjA0ODA1NjY2MH0.F9FsLxg8NA0nnYrYUnxPX7neXapNMEcBM2eM4ULztuc';
 export const supabase = self.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 export const login = async (email, password) => {
